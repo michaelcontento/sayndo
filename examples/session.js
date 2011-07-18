4cfe8ca14534da68858c8d66d89fa4e39f5d55ba
Tim Schindler <tim.schindler@adcloud.com>
2011-07-18 18:53:24 +0200

    commented session example

--- examples/session.js
+++ examples/session.js
@@ -6,11 +6,14 @@ var app = require('../lib/server.js');
 /*
  * Routes
  */
+
+// Create a session and set the auth type to "user".
 app.guest.GET['/'] = function(req, res) {
     res.createSession({authType: 'user'}, 1);
     res.render('/index.html', {msg: 'you have the auth type "guest"'});
 };
 
+// Destroy the current session.
 app.user.GET['/'] = function(req, res) {
     res.destroySession();
     res.render('/index.html', {msg: 'you have the auth type "user"'});
