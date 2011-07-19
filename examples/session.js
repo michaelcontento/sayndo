8cb96b40b04ebe47708a2605c256b81791550a95
Tim Schindler <tim.schindler@adcloud.com>
2011-07-19 17:09:00 +0200

    added cookie support

--- examples/session.js
+++ examples/session.js
@@ -9,12 +9,12 @@ var app = require('sayndo');
 
 // Create a session and set the auth type to "user".
 app.guest.GET['/'] = function(req, res) {
-    res.createSession({authType: 'user'}, 1);
+    res.session.write({authType: 'user'}, 1);
     res.render('/index.html', {msg: 'you have the auth type "guest"'});
 };
 
 // Destroy the current session.
 app.user.GET['/'] = function(req, res) {
-    res.destroySession();
+    res.session.remove();
     res.render('/index.html', {msg: 'you have the auth type "user"'});
 };
