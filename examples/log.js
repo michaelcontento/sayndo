7bdec1cb2eb7d69fd4e6d20cec98e57ac1d77948
Tim Schindler <tim.schindler@adcloud.com>
2011-07-19 14:24:03 +0200

    added colorized logs to app object

---
+++ examples/log.js
@@ -0,0 +1,10 @@
+/*
+ * Dependencies.
+ */
+var app = require('sayndo');
+
+app.log.success('Green success message');
+app.log.error('Red error message');
+app.log.info('Yellow success message');
+app.log.debug('Blue debug message');
+
