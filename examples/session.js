63909d5dcce3d2943f8bb4d63b31a843d9684610
Tim Schindler <tim.schindler@adcloud.com>
2011-07-19 18:47:23 +0200

    edited the examples and the Readme.md

--- examples/session.js
+++ examples/session.js
@@ -7,14 +7,25 @@ var app = require('sayndo');
  * Routes
  */
 
-// Create a session and set the auth type to "user".
+/*
+ * Here we create a session with the auth type 'user'. In
+ * this case the session is set for 1 minute. After that
+ * minute the users auth type falls back to the default
+ * auth type defined in lib/config.js
+ */
 app.guest.GET['/'] = function(req, res) {
+    // Just show the session object, if there is no one.
+    console.log(res.session);
+
     res.session.write({authType: 'user'}, 1);
     res.render('/index.html', {msg: 'you have the auth type "guest"'});
 };
 
 // Destroy the current session.
 app.user.GET['/'] = function(req, res) {
+    // Just show the session object if one is set.
+    console.log(res.session);
+
     res.session.remove();
     res.render('/index.html', {msg: 'you have the auth type "user"'});
 };
